30b76d56860d7ee9b943bf2097a0733fa83af571
ettoremaiorana <ettoremaiorana@tiscali.it>
2016-12-10 18:53:27 +0000

    Some changes in the API

--- src/main/java/com/fourcasters/forec/reconciler/query/IndexableDAO.java
+++ src/main/java/com/fourcasters/forec/reconciler/query/IndexableDAO.java
@@ -9,23 +9,37 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.TreeMap;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.BiFunction;
 
 public abstract class IndexableDAO {
 
+	private static final BiFunction <Long, Integer, Long> projectFirst = new BiFunction<Long, Integer, Long>() {
+		@Override
+		public Long apply(Long t, Integer u) {
+			return t;
+		}
+	};
+	private static final BiFunction<Long, Integer, Long> sum = new BiFunction<Long, Integer, Long>() {
+		@Override
+		public Long apply(Long t, Integer u) {
+			return t + u.longValue();
+		}
+	};
 	private final Map<String, TreeMap<Long, Long>> indexes = new HashMap<>(16);
 
+	public abstract Path getRootPath();
 	public abstract Path getFilePath(String cross);
 	public abstract RecordBuilder getRecordBuilder(String recordFormat);
 	public abstract RecordBuilder getRecordBuilder();
 	public abstract void onIndexingEnd(String cross);
 	public abstract String getDefaultFormat();
 
-	protected TreeMap<Long, Long> javaDbHash(String cross) throws IOException {
+	protected TreeMap<Long, Long> createIndex0(String cross) throws IOException {
 		String format = getDefaultFormat();
-		return javaDbHash(cross, format);
+		return createdIndex0(cross, format);
 	}
 
-	protected TreeMap<Long, Long> javaDbHash(String cross, String recordFormat) throws IOException {
+	protected TreeMap<Long, Long> createdIndex0(String cross, String recordFormat) throws IOException {
 		cross = cross.toLowerCase();
 		TreeMap<Long, Long> hash = new TreeMap<>();
 		final Path path = getFilePath(cross);
@@ -42,23 +56,23 @@ public abstract class IndexableDAO {
 		return hash;
 	}
 
-	public boolean dbhash(String cross, String format) throws IOException {
-		boolean result = indexes.put(cross, javaDbHash(cross, format)) == null;
-		onIndexingEnd(cross);
+	public boolean createIndex(String table, String recordFormat) throws IOException {
+		boolean result = indexes.put(table, createdIndex0(table, recordFormat)) == null;
+		onIndexingEnd(table);
 		return result;
 	}
-	public boolean dbhash(String cross) throws IOException {
-		return indexes.put(cross, javaDbHash(cross)) == null;
+	public boolean createIndex(String table) throws IOException {
+		return indexes.put(table, createIndex0(table)) == null;
 	}
-	public int hashCount(String cross) {
-		TreeMap<Long, Long> index = indexes.get(cross);
+	public int indexSize(String table) {
+		TreeMap<Long, Long> index = indexes.get(table);
 		if (index == null) {
 			return -1;
 		}
 		return index.size();
 	}
 
-	public long offset(String cross, Date date, boolean exact) throws IOException {
+	private long offset(String cross, Date date, BiFunction<Long, Integer, Long> f) throws IOException {
 		long timestamp = date.getTime();
 
 		TreeMap<Long, Long> index = indexes.get(cross);
@@ -76,8 +90,7 @@ public abstract class IndexableDAO {
 			@Override
 			protected boolean onNewRecord(Record curr, Record prev, long bytesReadSoFar, int recordLength) {
 				if (curr.index() >= timestamp) {
-					offset.set(exact ? bytesReadSoFar + recordLength
-									 : bytesReadSoFar);
+					offset.set(f.apply(bytesReadSoFar, recordLength));
 					return false;
 				}
 				return true;
@@ -86,9 +99,14 @@ public abstract class IndexableDAO {
 		return offset.get();
 	}
 
-	public abstract Path getRootPath();
+	public long offset(String cross, Date date) throws IOException {
+		return offset(cross, date, projectFirst);
+	}
 
-	public void dbhashAll(String format) throws IOException {
+	public long offsetPlusOne(String cross, Date date) throws IOException {
+		return offset(cross, date, sum);
+	}
+	public void createIndexAll(String format) throws IOException {
 		final File root = getRootPath().toFile();
 		if (!root.isDirectory()) {
 			throw new IllegalArgumentException("Unable to read table files from non directory " + root);
@@ -99,12 +117,12 @@ public abstract class IndexableDAO {
 			if (pos > 0) {
 				cross = cross.substring(0, pos);
 			}
-			dbhash(cross, format);
+			createIndex(cross, format);
 		}
 	}
 
-	public void dbhashAll() throws IOException {
+	public void createIndexAll() throws IOException {
 		String format = getDefaultFormat();
-		dbhashAll(format);
+		createIndexAll(format);
 	}
 }
